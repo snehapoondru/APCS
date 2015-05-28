a19109a0efa81a509b6491c4ef17712aa7e7df36
snehapoondru <spoondr7@students.d125.org>
2015-05-28 07:26:41 -0500

    added Driver

---
+++ Project/Driver.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Driver here.
+ * 
+ * @author (Sneha) 
+ * @version (5/27/15)
+ */
+import java.util.Scanner;
+public class Driver
+{
+    public static void main (String args[])
+    {
+        System.out.println("Welcome to a game of number hangman! Enter the length of the number you wish to guess: "); 
+        Scanner scan = new Scanner(System.in); 
+        int length = scan.nextInt();
+        Number secret = new Number(length); 
+        System.out.println("
+    }
+
+    
+}
